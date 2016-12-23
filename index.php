897d3388170ea3270af66a09afd9264a1d8b2241
Martin Kučera <m.spidy@gmail.com>
2016-12-23 14:57:06 +0100

    Using Zomato API

--- index.php
+++ index.php
@@ -1,75 +1,37 @@
 <?php
 
+define('API_URL', 'https://developers.zomato.com/api/v2.1');
+define('API_KEY', '938c81de2f543a03f1d05f2d6ba54f39');
+
+use Nette\Utils\Json;
+
 header('Content-Type:text/html;charset=utf-8');
 require_once __DIR__ . '/vendor/autoload.php';
 Tracy\Debugger::enable(Tracy\Debugger::DETECT, __DIR__ . '/log');
 
-function download_menu($url) {
+function download_menu($id) {
 
 	$ch = curl_init();
 
-	curl_setopt($ch, CURLOPT_URL, $url);
+	curl_setopt($ch, CURLOPT_URL, API_URL . "/dailymenu?res_id=$id");
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 	curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET");
-	
-	
-	$headers = array();
-	$headers[] = "Pragma: no-cache";
-	$headers[] = "Dnt: 1";
-	$headers[] = "Accept-Encoding: deflate, sdch, br";
-	$headers[] = "Accept-Language: en,cs;q=0.8,en-GB;q=0.6";
-	$headers[] = "Upgrade-Insecure-Requests: 1";
-	$headers[] = "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.98 Safari/537.36";
-	$headers[] = "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8";
-	$headers[] = "Cache-Control: no-cache";
-	$headers[] = "Authority: www.zomato.com";
-	$headers[] = "Cookie: ak_bmsc=882745FCADD9468899DC6D37AFA9546B6867493F9C76000024364558C33F4F38~plQAqfVwtSDb+GEeBa7Bl6owISjtG1VOE4cZQDrVYI+sgz0aA8kJ9ox/r1twnwd9B1voFQPB/goQrEbj5mlX8Wa9LBZkoSf3L+jkpFBes6dURf9b2xBsEyz3SWeF5hD2tibpzQIUSi0T28V9048VLpA313kqg1fljLz6AlAL28sLy+VcDLqCZDknm92jbIa1ohTXiuMKIhL2KfqEp6P8zj+A==";
-	curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+
+	curl_setopt($ch, CURLOPT_HTTPHEADER, [
+		'Accept: application/json',
+		'user_key: ' . API_KEY,
+	]);
 
 	$result = curl_exec($ch);
 	if (curl_errno($ch)) {
 	    echo 'Error:' . curl_error($ch);
 	}
 	curl_close ($ch);
-	return $result;
-}
-
-function utf8Url($url) {
-	if(strpos($url, '%') !== FALSE) return $url;
-	list($protocol, $rest) = explode('://', $url);
-	$parts = explode('/', $rest);
-	$parts = array_map('rawurlencode', $parts);
-	return $protocol . '://' . implode('/', $parts);
-}
-
-function fromZomato($url, $force = FALSE) {
-	$url = utf8Url($url);
-
-	$cachePath = __DIR__ . '/cache/temp-' . date('Y-m-d') . '-' . md5($url) . '.html';
-
-	if(!$force && file_exists($cachePath)) {
-		return file_get_contents($cachePath);
-	}
-	$html = download_menu($url);
-
-	$document = new DOMDocument();
-	$document->loadHTML($html);
-	$xpath = new DOMXPath($document);
-	$nodes = $xpath->query('//div[contains(concat(" ", normalize-space(@class), " "), " tmi-group ")]');
-	$node = $nodes->item(0);
-
-	$content = "";
-
-	if($nodes->length && $node->ownerDocument) {
-		$content = $node->ownerDocument->saveHTML($node);
-	}
-	file_put_contents($cachePath, $content);
-
-	return $content;
+	return Json::decode($result, Json::FORCE_ARRAY)['daily_menus'][0]['daily_menu'];
 }
 
 $group = empty($_GET['g']) ? "mango" : $_GET['g'];
-$db = json_decode(file_get_contents(__DIR__ . '/db.json'), TRUE);
+$db = Json::decode(file_get_contents(__DIR__ . '/db.json'), Json::FORCE_ARRAY);
 
 $sources = $db[$group];
 
@@ -92,19 +54,37 @@ $sources = $db[$group];
 <?php
 
 $first = TRUE;
-foreach($sources as $name => $url):
-	$content = fromZomato($url, isset($_GET['force']));
+foreach($sources as $name => $id):
+	$data = download_menu($id);
 ?>
-	<div class="restaurant<?php echo $first ? ' is-open' : ''; echo empty($content) ? ' is-empty' : '';?>">
-		<h2 class="restaurant-name"><a href="<?php echo htmlspecialchars($url) ?>"><?php echo htmlspecialchars($name) ?></a></h2>
+	<div class="restaurant<?php echo $first ? ' is-open' : ''; echo empty($data) ? ' is-empty' : '';?>">
+		<h2 class="restaurant-name"><a href="#"><?php echo htmlspecialchars($name) ?></a></h2>
 		<div class="restaurant-menu">
+			<div class="tmi-group mtop">
+				<div class="tmi-group-name bold fontsize3 pb5 bb"><?php echo date('j. n. Y', strtotime($data['start_date'])) ?></div>
+	<?php if(empty($data)):	?>
+		<div class="empty-menu">Chyba</div>
 <?php
-if(empty($content)) {
-	?><div class="empty-menu"><a href="?force&g=<?php echo $group ?>" class="refresh-btn">Zkusit načíst znovu</a></div><?php
-} else {
-	echo $content;
-}
+	else:
+		foreach ($data['dishes'] as $dish):
+			$dish = $dish['dish'];
+	        ?>
+				<div class="tmi tmi-daily pb5 pt5  ">
+					<div class="tmi-text-group col-l-14 col-s-14">
+						<div class="row">
+							<div class="tmi-name"><?php echo $dish['name']; ?></div>
+						</div>
+					</div>
+					<div class="tmi-price ta-right col-l-2 col-s-2 bold600">
+						<div class="row"><?php echo $dish['price']; ?></div>
+					</div>
+					<div class="clear"></div>
+				</div>
+			 <?php
+		endforeach;
+		endif;
 ?>
+			</div>
 		</div>
 	</div>
 <?php $first = FALSE; endforeach; ?>
